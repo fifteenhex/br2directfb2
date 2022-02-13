41c7ea4135e68880d4dc3cdf07916045d4d05c5d
Daniel Palmer <daniel@0x0f.com>
2022-02-13 22:28:59 +0900

    Bit of tweaking. Try to make sure we build with optimisation turned on.

--- package/directfb2/directfb2.mk
+++ package/directfb2/directfb2.mk
@@ -11,22 +11,36 @@ DIRECTFB2_DEPENDENCIES = host-flux
 DIRECTFB2_LICENSE = LGPL-2.1
 DIRECTFB2_INSTALL_STAGING = YES
 
+# ignore the -Os option, this is performance critical
+DIRECTFB2_CFLAGS = $(TARGET_CFLAGS) -O3
+DIRECTFB2_CXXFLAGS = $(TARGET_CFLAGS) -O3
+
+ifeq ($(BR2_GCC_ENABLE_LTO),y)
+DIRECTFB2_CFLAGS += -flto
+DIRECTFB2_CXXFLAGS += -flto
+endif
+
+ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+DIRECTFB2_CFLAGS += -mfpu=neon
+DIRECTFB2_CXXFLAGS += -mfpu=neon
+endif
+
 ifeq ($(BR2_PACKAGE_DIRECTFB2_MULTI),y)
-	DIRECTFB2_CONF_OPTS += -Dmulti=true
+DIRECTFB2_CONF_OPTS += -Dmulti=true
 else
-	DIRECTFB2_CONF_OPTS += -Dmulti=false
+DIRECTFB2_CONF_OPTS += -Dmulti=false
 endif
 
 ifeq ($(BR2_PACKAGE_DIRECTFB2_DRMKMS),y)
-	DIRECTFB2_CONF_OPTS += -Ddrmkms=true
+DIRECTFB2_CONF_OPTS += -Ddrmkms=true
 else
-	DIRECTFB2_CONF_OPTS += -Ddrmkms=false
+DIRECTFB2_CONF_OPTS += -Ddrmkms=false
 endif
 
 ifeq ($(BR2_PACKAGE_DIRECTFB2_FBDEV),y)
-	DIRECTFB2_CONF_OPTS += -Dfbdev=true
+DIRECTFB2_CONF_OPTS += -Dfbdev=true
 else
-	DIRECTFB2_CONF_OPTS += -Dfbdev=false
+DIRECTFB2_CONF_OPTS += -Dfbdev=false
 endif
 
 $(eval $(meson-package))
