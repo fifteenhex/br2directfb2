7526730a400c88b3a60f5e886f39f40f249e1799
Daniel Palmer <daniel@0x0f.com>
2022-11-13 16:25:58 +0900

    hacks: sdl2: build dfb driver in statically if dfb2 is selected

--- package/sdl2/sdl2.mk
+++ package/sdl2/sdl2.mk
@@ -1,4 +1,4 @@
 ifeq ($(BR2_PACKAGE_SDL2_DIRECTFB2),y)
 SDL2_DEPENDENCIES += directfb2
-SDL2_CONF_OPTS += --enable-video-directfb
+SDL2_CONF_OPTS += --enable-video-directfb --disable-directfb-shared
 endif
